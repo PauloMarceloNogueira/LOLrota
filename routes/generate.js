af69c413c694f338c308b6c45d3ed52cf4b132f6
Santiago Oliveira <santiagooliveira@MacBook-Pro-de-Santiago.local>
2015-10-26 15:21:21 -0200

    Problem with count in generate and change text in index

--- routes/generate.js
+++ routes/generate.js
@@ -5,16 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   	var Basewords = new Array("Lane","top","mid","Bot","AD","Harass","Push","Tower Dive","Tower Hug");
 
-	var Actionwords = new Array("Ganko","deu Push","estava na Lane","upou","tankou","farmou","upou","estava de Jungle","feedou","fez um call","counterou","levou um burst","ultou","deu um backdoor","");
+	var Actionwords = new Array("Ganko","deu Push","estava na Lane","upou","tankou","farmou","upou","estava de Jungle","feedou","fez um call","counterou","levou um burst","ultou","deu um backdoor");
 
 	var Itenswords = new Array("o Blue","o Red","o Jungle deles","a torre do bot","a torre do mid","o inibidor","o Drag","o Barão");
 
 	var Characterswords = new Array("O Aatrox","A Ahri","A Akali","O Amummu","A Annie", "O Darius", "O Draven", "O Garen", "A Elise", "A Leona","A Lulu");
 
-	var randBase = Math.floor((Math.random() * (Basewords.length - 1)) + 1);
-	var randAction = Math.floor((Math.random() * (Actionwords.length - 1)) + 1);
-	var randItens = Math.floor((Math.random() * (Itenswords.length - 1)) + 1);
-	var randCharacters = Math.floor((Math.random() * (Characterswords.length - 1)) + 1);
+	var randBase = Math.floor((Math.random() * (Basewords.length - 1)));
+	var randAction = Math.floor((Math.random() * (Actionwords.length - 1)));
+	var randItens = Math.floor((Math.random() * (Itenswords.length - 1)));
+	var randCharacters = Math.floor((Math.random() * (Characterswords.length - 1)));
 
 	console.log(Characterswords[randCharacters]+' '+Actionwords[randAction]+" pra pegar "+ Itenswords[randItens]);
  
